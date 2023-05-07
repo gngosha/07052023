62e38166f115344df5c61e7b28a32420062f7592
SupaFatGeorge <georgy.fabio2017@yandex.ru>
2023-05-07 16:40:06 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,54 +1,16 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-// export default defineConfig({
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'index.html'),
-//         nested: resolve(__dirname, 'nested/text.html'),
-//       },
-//     },
-//     target: 'esnext',
-//   },
-// })
-
-// vite.config.js
-
-// vite.config.js
 export default defineConfig({
-    build: {
-      outDir: 'dist'
-    },
-    optimizeDeps: {
-      include: ['leaflet', 'axios']
-    },
-    // Configure pages
-    pages: {
-      index: {
-        // entry for the page
-        entry: 'client/index.html',
-        // the source template
-        template: 'client/index.html',
-        // output as dist/index.html
-        filename: 'index.html'
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/text.html'),
       },
-      map: {
-        // entry for the page
-        entry: 'client/map.html',
-        // the source template
-        template: 'client/map.html',
-        // output as dist/map/index.html
-        filename: 'map/index.html'
-      },
-      text: {
-        // entry for the page
-        entry: 'client/text.html',
-        // the source template
-        template: 'client/text.html',
-        // output as dist/text/index.html
-        filename: 'text/index.html'
-      }
-    }
-  })
+    },
+    target: 'esnext',
+  },
+})
+
   
