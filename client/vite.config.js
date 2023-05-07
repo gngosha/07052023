550ee69f911e31cdc179110be337e3a0a68d2715
SupaFatGeorge <georgy.fabio2017@yandex.ru>
2023-05-07 16:31:20 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,52 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+// import { resolve } from 'path'
+// import { defineConfig } from 'vite'
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/text.html'),
-      },
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         nested: resolve(__dirname, 'nested/text.html'),
+//       },
+//     },
+//     target: 'esnext',
+//   },
+// })
+
+// vite.config.js
+export default {
+    build: {
+      outDir: 'dist'
     },
-    target: 'esnext',
-  },
-})
+    optimizeDeps: {
+      include: ['leaflet', 'axios']
+    },
+    // Configure pages
+    pages: {
+      index: {
+        // entry for the page
+        entry: 'client/index.html',
+        // the source template
+        template: 'client/index.html',
+        // output as dist/index.html
+        filename: 'index.html'
+      },
+      map: {
+        // entry for the page
+        entry: 'client/map.html',
+        // the source template
+        template: 'client/map.html',
+        // output as dist/map/index.html
+        filename: 'map/index.html'
+      },
+      text: {
+        // entry for the page
+        entry: 'client/text.html',
+        // the source template
+        template: 'client/text.html',
+        // output as dist/text/index.html
+        filename: 'text/index.html'
+      }
+    },
+  }
+  
