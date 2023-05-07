8249aae863df32f3a9e4082ad0aa477c28de12f2
SupaFatGeorge <georgy.fabio2017@yandex.ru>
2023-05-07 10:26:12 +0300

    first commit

---
+++ client/map.js
@@ -0,0 +1,138 @@
+// import { riddle, country } from "../server/server"
+
+// fetch API from server
+const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+          'Content-Type': 'application/json',
+    }
+})
+
+let country;
+let riddle;
+
+if (response.ok) {
+    const data = await response.json();
+    riddle = data.bot // trims any trailing spaces/'\n' ;
+    country = data.message;
+    console.log(riddle);
+    console.log(country)
+} else {
+    const err = await response.text()
+
+    console.log("Something went wrong")
+    console.log(err)
+}
+
+document.getElementById('bottomGuess').innerHTML = `${riddle}`
+console.log(country)
+let k = 3;
+let c = 2;
+document.querySelectorAll(".allPaths").forEach(e => {
+    e.addEventListener("mouseover", async function () {
+        // window.onmousemove=function (j) {
+        //      let x = j.clientX
+        //      let y = j.clientY
+        //      document.getElementById('name').style.top = y -60 + 'px'
+        //      document.getElementById('name').style.left = x + 'px'
+        // }
+         
+             
+             if (k !== 0) {
+                e.style.cursor = 'pointer';
+                e.style.fill = "#00ff95";
+                document.getElementById('try').innerHTML = `Осталось попыток: ${k} <br>
+                Выбрано: ${e.id}`
+             }
+             
+             if (k === 0) {
+                e.style.cursor = 'default';
+             }
+        //  document.getElementById("name").style.opacity = 1
+        
+        //  document.getElementById("namep").innerText = e.id
+    })
+    e.addEventListener("mouseleave", function () {
+            if (k !== 0) {
+                document.getElementById('try').innerHTML = `Осталось попыток: ${k} <br> *Выберите страну*`;
+                e.style.fill = "white";
+            }
+            
+            
+        
+        //  document.getElementById("name").style.opacity = 0
+    })
+    
+    
+    e.addEventListener('click', function (e) {
+        e.preventDefault();
+        c = c - 1;
+        if (c === -2) {
+            window.location.reload();
+        }
+        if (c <= 1 && c !== -2) {
+            //Show loader
+            // document.getElementById('loading').style.display='block';
+            setTimeout(GuessCountry);
+        }
+    })
+
+    function GuessCountry(){
+        // UI Vars
+        
+        if (country === e.id) {
+            document.getElementById('try').innerHTML = `Угадали. Страна - ${country}`;
+            k = 0;
+            document.getElementById('try').classList = 'tries-success input-group-addon card card-body text-center mt-1 mb-2';
+            document.getElementById('try').classList.add('text-white');
+            e.style.fill = 'green'
+                setTimeout(function(){
+                    //window.location.assign("text.html");
+                    window.location.reload();
+                 }, 4000);
+        } else {
+            e.style.fill = 'red'
+            k = k - 1
+            if (k !== 0) {
+                document.getElementById('try').innerHTML = `Не угадали! <br> *Выберите страну*`;
+                document.getElementById('try').classList.add('not-yet');
+            }
+            if (k === 0) {
+                document.getElementById('try').innerHTML = `Слили. Страна - ${country}`;
+                document.getElementById('try').classList = 'tries-failure input-group-addon card card-body text-center mt-1 mb-2';
+                document.getElementById('try').classList.add('text-white');
+                e.style.fill = 'red'
+                setTimeout(function(){
+                    //window.location.assign("text.html");
+                    window.location.reload();
+                 }, 4000);
+            }
+    
+        }
+
+      
+        } 
+
+
+
+
+})
+        // if (k !== 0) {
+        //     if (e.id === country) {
+        //         e.style.fill = 'green'
+        //         setTimeout(function(){
+        //             window.location.assign("text.html");
+        //          }, 4000);
+        //     } else {
+        //         e.style.fill = 'red'
+        //     }
+        // }
+        // if (k === 0) {
+        //     e.style.fill = 'red'
+        //         setTimeout(function(){
+        //             window.location.assign("text.html");
+        //          }, 4000);
+        // }
+
+        // k -= 1;
+
---
+++ client/script.js
@@ -0,0 +1,121 @@
+// import { country } from '../server/server.js'
+
+// Listen for submit
+const amount = document.getElementById('amount');
+amount.value = '';
+let down = document.getElementById('down');
+let tries = document.getElementById('tries');
+let k = 3;
+
+let reload = document.getElementById('reload');
+
+let generated = document.getElementById("generated");
+
+// trying to fetch data
+
+// const data = new FormData(form);
+
+const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+          'Content-Type': 'application/json',
+    }
+})
+
+let country;
+let riddle;
+
+if (response.ok) {
+    const data = await response.json();
+    riddle = data.bot // trims any trailing spaces/'\n' ;
+    country = data.message;
+    console.log(riddle);
+    console.log(country)
+} else {
+    const err = await response.text()
+
+    console.log("Something went wrong")
+    console.log(err)
+}
+
+generated.innerHTML = `${riddle}`;
+
+amount.disabled = true;
+// generated.innerHTML = riddle;
+amount.disabled = false;
+down.classList = "lol card card-body text-center mt-5";
+console.log(country);
+let c = 1;
+if (generated.innerHTML !== '*Генерация страны*') {
+  document.getElementById('forn-group').innerHTML += `<input type="submit" value="Угадать!" class="guess btn btn-info btn-block mb-1" id="btn">
+  <a href="./map.html" class="redirect nounderline "><input type="button" value="Угадать на карте!" class="guess btn btn-info btn-block" id="map"></a>
+  `
+}
+
+document.getElementById('btn').addEventListener('click', function(e){
+    e.preventDefault();
+    c = c - 1;
+    amount.disabled = false;
+    //Hide results
+    document.getElementById('results').style.display='none';
+
+    if (c <= 0) {
+      //Show loader
+      // document.getElementById('loading').style.display='block';
+      setTimeout(calculateResults);
+    }
+  });
+  
+  // Calculate Results
+  function calculateResults(){
+    // UI Vars
+    const monthlyPayment = document.getElementById('monthly-payment');
+    monthlyPayment.classList = "form-control text-light"
+    
+    const answer = country.toLowerCase();
+    const principal = amount.value.toLowerCase();
+
+    if (principal === answer) {
+        amount.disabled = true;
+        document.getElementById('btn').disabled = true;
+        document.getElementById('map').disabled = true;
+        monthlyPayment.value =`Да! Это ${country}`;
+        monthlyPayment.classList.add("bg-success");
+        monthlyPayment.classList.add("tries-success");
+        tries.innerHTML = 'Вы выиграли!';
+        tries.classList = "input-group-addon mb-3";
+        reload.innerHTML = 'Перезагрузка через 10 секунд...';
+        reload.classList = "input-group-addon bg-warning text-danger mb-3"
+        setTimeout(function(){
+            window.location.reload();
+         }, 4000);
+    } else {
+        monthlyPayment.value =`Нет...`;
+        monthlyPayment.classList.add("bg-danger");
+        k = k - 1
+        tries.innerHTML = `Осталось попыток: ${k}`;
+        if (k === 0) {
+            amount.disabled = true;
+            document.getElementById('btn').disabled = true;
+            document.getElementById('map').disabled = true;
+            tries.innerHTML = 'Вы проиграли!';
+            tries.classList = "input-group-addon mb-3"
+            monthlyPayment.value =`Нет... Это ${country}`;
+            monthlyPayment.classList.add("bg-danger");
+            monthlyPayment.classList.add("tries-failure");
+            monthlyPayment.classList.add("border-danger");
+            reload.innerHTML = 'Перезагрузка через 10 секунд...';
+            reload.classList = "input-group-addon bg-warning text-danger mb-3"
+            setTimeout(function(){
+                window.location.reload();
+             }, 4000);
+        }
+
+    }
+      //show results
+      document.getElementById('results').style.display='block';
+  
+      //hide loader
+      // document.getElementById('loading').style.display='none';
+  
+    } 
