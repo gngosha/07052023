1ae72ee8b7fa5182327f04eac8c03bb2143b560d
SupaFatGeorge <georgy.fabio2017@yandex.ru>
2023-06-06 00:37:26 +0300

    added new files to viter config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         nested: resolve(__dirname, 'nested/text.html'),
-        anotherNested: resolve(__dirname, 'nested/map.html')
+        anotherNested: resolve(__dirname, 'nested/map.html'),
+        tableBody: resolve(__dirname, 'table/body.html'),
+        tableMenu: resolve(__dirname, 'table/menu.html'),
+        tablesVVP: resolve(__dirname, 'table/tables/vvp.html'),
+        tablesICHR: resolve(__dirname, 'table/tables/ichr.html'),
+        tablesPopulation: resolve(__dirname, 'table/tables/population.html')
       },
     },
     target: 'esnext',
